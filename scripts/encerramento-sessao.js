172c0c195fe705c42693c4aefef31f7dc2b13f79
Vitor Souza <souza.vitor1480@gmail.com>
2025-12-21 01:53:05 -0300

    encerramento-sessao.js atualizado

--- scripts/encerramento-sessao.js
+++ scripts/encerramento-sessao.js
@@ -1,4 +1,4 @@
-import { enviarDados } from "./utilitarios.js";
+import { enviarDados } from './utilitarios.js';
 
 window.addEventListener('pageshow', async function () {
     const { sucesso, dados, mensagem } = await enviarDados('POST', 'encerrar-sessao.php', { acao: 'encerrar-sessao' });
