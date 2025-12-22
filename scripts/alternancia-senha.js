5fc82aac955f3823d669261af92c8ed389395f9e
Vitor Souza <souza.vitor1480@gmail.com>
2025-12-21 21:39:22 -0300

    alternancia-senha,js e utilitarios.js atualizados

--- scripts/alternancia-senha.js
+++ scripts/alternancia-senha.js
@@ -1,9 +1,9 @@
-import { senha, alternarIcones } from './utilitarios.js';
+import { campoSenha, alternarIcones } from './utilitarios.js';
 
 const botaoAlternarSenha = document.querySelector('#botao-alternar-senha');
 const iconeAlternarSenha = document.querySelector('#icone-alternar-senha');
 
 botaoAlternarSenha.addEventListener('click', function () {
-    senha.type = senha.type === 'password' ? 'text' : 'password';
-    alternarIcones(senha.type !== 'password', iconeAlternarSenha, 'bi-eye', 'bi-eye-slash');
+    campoSenha.type = senha.type === 'password' ? 'text' : 'password';
+    alternarIcones(campoSenha.type !== 'password', iconeAlternarSenha, 'bi-eye', 'bi-eye-slash');
 });
